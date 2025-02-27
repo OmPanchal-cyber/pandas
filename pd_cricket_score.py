7744088c608c362bd2bc4f05f495ac1f7c2bedd5
OmPanchal-cyber <panchalom2114@gmail.com>
2025-02-27 21:46:03 +0530

    Update pd_cricket_score.py

--- pd_cricket_score.py
+++ pd_cricket_score.py
@@ -1,46 +1,23 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[105]:
-
 
 import pandas as pd
-
-
-# In[106]:
-
-
 data=pd.read_excel(r"C:\Users\panchal_____om\Downloads\espn cric score (1).xlsx",header=3)
-
-
-
-# In[123]:
-
-
 data
 
 data["SR"].unique
-
 data[data["SR"]=="-"].index
-
 data.drop(data[data["SR"]=="-"].index,inplace=True)
-
 data["SR"].unique
-
 data.reset_index(drop="index",inplace=True)
 
 data
-
 data.select_dtypes(include="object").columns
 
 data[['Mat','HS','BF','4s','6s']]=data[['Mat','HS','BF','4s','6s']].replace("[*+]",'',regex=True).astype(int)
-
 data[["SR"]]=data[["SR"]].replace("[*+]",'',regex=True).astype(float)
 
 data.info()
 
 data["Country"]=data["Player"].str.split(" ").str[-1].replace("[()]","",regex=True)
-
 data["Player"]=data["Player"].str.split("(").str[0]
 
 data.info()
@@ -53,6 +30,5 @@ data
 data.info()
 
 data.drop("Span",axis=1,inplace=True)
-
 data
 
